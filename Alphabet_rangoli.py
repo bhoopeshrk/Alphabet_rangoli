4deb284605e6ad0ef1b3827ad27dcf2aab6f9c17
Bhoopesh R K <64587796+bhoopeshrk@users.noreply.github.com>
2021-07-17 16:07:19 +0530

    Add files via upload

---
+++ Alphabet_rangoli.py
@@ -0,0 +1,14 @@
+import string
+def print_rangoli(size):
+    alpha = string.ascii_lowercase
+    
+    L = []
+    for i in range(size):
+        s = "-".join(alpha[i:size])
+        L.append((s[::-1]+s[1:]).center(4*size-3, "-"))
+        
+    print('\n'.join(L[::-1]+L[1:]))
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
